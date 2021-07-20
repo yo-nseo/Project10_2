78671da7559d93a2017976996e3603f22d1eb54f
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-07-20 09:34:59 +0900

    Completion ResultActivity.java Commit

--- app/src/main/java/kr/hs/emirim/ohyoonseo/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project10_2/MainActivity.java
@@ -2,8 +2,10 @@ package kr.hs.emirim.ohyoonseo.project10_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -31,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        Button btnFinish = findViewById(R.id.btn_done);
+        btnFinish.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("voteCount", voteCount);
+            intent.putExtra("imgNames", imgNames);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/kr/hs/emirim/ohyoonseo/project10_2/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project10_2/ResultActivity.java
@@ -2,7 +2,12 @@ package kr.hs.emirim.ohyoonseo.project10_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        int[] voteCount = intent.getIntArrayExtra("voteCount");
+        String[] imgNames = intent.getStringArrayExtra("imgNames");
+
+        TextView[] tv = new TextView[imgNames.length];
+        RatingBar[] rb = new RatingBar[imgNames.length];
+
+        int[] tvIds = {R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6, R.id.text7, R.id.text8, R.id.text9};
+        int[] rbIds = {R.id.rating1, R.id.rating2, R.id.rating3, R.id.rating4, R.id.rating5, R.id.rating6, R.id.rating7, R.id.rating8, R.id.rating9};
+
+        for (int i = 0; i < tv.length; i++){
+            tv[i] = findViewById(tvIds[i]);
+            rb[i] = findViewById(rbIds[i]);
+            tv[i].setText(imgNames[i]);
+            rb[i].setRating(voteCount[i]);
+        }
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
